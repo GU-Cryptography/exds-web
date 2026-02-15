4312d812b375a2913a236028a860b29fd08097f1
haijiangxu <haijiangxu@hotmail.com>
2026-02-15 18:32:39 +0800

    refactor: 优化目录文件结构

--- webapp/check_mp.py
+++
@@ -1,9 +0,0 @@
-from webapp.tools.mongo import DATABASE
-import sys
-
-# Add project root to path
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-RAW_MP_DATA = DATABASE['raw_mp_data']
-sample = RAW_MP_DATA.find_one()
-print(sample)
